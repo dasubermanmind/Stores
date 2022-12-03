4dfd59935b3d7e3f127b4aaca89e7827b33dcb03
dasubermanmind <ero33@drexel.edu>
2022-12-03 14:08:40 -0500

    Updated docker, updated the endpoints/added defensive programming to make sure each error is caught appropriately

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 1. `pip install -r requiremnts.txt`
 1. flask run
 
+
+## Docker setup (optional)
+1. Run `docker build -t rest-store-api` . 
+1. Once the container has been built we then run it via `docker run -dp 5000:5000 rest-store-api`
+
+
+
 Now you can test each individual endpoints independantly.
 
 ## Todo's
@@ -12,7 +19,7 @@ Now you can test each individual endpoints independantly.
 1. Add Users Functionality []
 1. Add middleware JWT []
 1. Add SQLAlchemy []
-1. Integrate with Postgres
-1. Integrate with Docker
-1. Integrate with Celery
+1. Integrate with Postgres []
+1. Integrate with Docker [X]
+1. Integrate with Celery []
 1. Maybe add the SIMPLETL as part of the process?? [] 
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import uuid
 from flask import Flask, request
 from db.db import stores, items
+from flask_smorest import abort
+
 
 app = Flask(__name__)
 
@@ -12,9 +14,19 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
-    store_id = uuid.uuidv4().hex
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
+    store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
+
     return store
 
 
@@ -23,14 +35,36 @@ def get_store(id):
     try:
         return stores[id]
     except KeyError as e:
-        return {"message": "store not found"}
+        abort(404, message="Store not found")
+        
+        
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
     
 
-@app.post("/store")
+@app.post("/item")
 def create_item():
     item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -48,18 +82,31 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
-    
-@app.post("/item")
-def create_item():
+        abort(404, message="Item not found")    
+        
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
     item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+        )
+    try:
+        item = items[item_id]
+        item |= item_data
 
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
 
-    return item
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
     
     
