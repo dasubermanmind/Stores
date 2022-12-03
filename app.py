e640d8ca773137d7af3a0c21bf293a8edc0e8a4e
dasubermanmind <ero33@drexel.edu>
2022-12-03 13:58:53 -0500

    Refactor endpoints

--- app.py
+++ app.py
@@ -1,39 +1,65 @@
+import uuid
 from flask import Flask, request
+from db.db import stores, items
 
 app = Flask(__name__)
-
-stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
 
 # app decorator registers the endpoint with the HTTP verb at the specific endpoint
 @app.get("/store")
 def get_stores():
-    return {'stores': stores}
+    return {'stores': list(stores.values())}
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuidv4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store
 
 
-@app.get("/store/<string:name>")
-def get_store(name: str):
-    for store in stores:
-        if store['name'] == name:
-            return store
+@app.get("/store/<string:id>")
+def get_store(id):
+    try:
+        return stores[id]
+    except KeyError as e:
+        return {"message": "store not found"}
     
-    return {"message": 'No store found'}, 404
 
-@app.post("/store/<string:name>")
-def create_item(name: str):
-    request_data = request.get_json
+@app.post("/store")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+
+@app.get('/items')
+def get_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
     
-    for store in stores:
-        if store["name"] == name:
-            new_item ={"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
     
-    return {"message": "Store not Found"}, 404
     
