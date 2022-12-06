f771eca46b19fbcd1d3c1c40af37ee9f6ae23663
dasubermanmind <ero33@drexel.edu>
2022-12-06 15:39:17 -0500

    Added store/item blueprint

--- api/resources/store.py
+++ api/resources/store.py
@@ -16,9 +16,27 @@ class Store(MethodView):
         except KeyError as e:
             abort(404, message="Store not found")
     
+    @blp.route('/store/<string:store_id>')
     def delete(self, store_id):
         try:
             del stores[store_id]
+            return {"message": "Store deleted."}
+        except KeyError:
+            abort(404, message="Store not found.")
+            
+
+class Item(MethodView):
+    @blp.route('/item/<string:item_id>')
+    def get(self, item_id):
+        try:
+            return stores[item_id]
+        except KeyError as e:
+            abort(404, message="Item not found")
+    
+    @blp.route('/item/<string:item_id>')
+    def delete(self, item_id):
+        try:
+            del stores[item_id]
             return {"message": "Store deleted."}
         except KeyError:
             abort(404, message="Store not found.")
