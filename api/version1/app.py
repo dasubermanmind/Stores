231ff406552c41d3853ceed15efdca072a279d65
dasubermanmind <ero33@drexel.edu>
2022-12-06 20:35:06 -0500

    Added openAPI docs

--- api/version1/app.py
+++ api/version1/app.py
@@ -1,10 +1,25 @@
 import uuid
 from flask import Flask, request
 from db.db import stores, items
-from flask_smorest import abort
+from flask_smorest import abort, Api
 
+from resources.store import blp as StoreBlueprint
 
 app = Flask(__name__)
+
+
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+api = Api(app)
+
+# api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
 
 # app decorator registers the endpoint with the HTTP verb at the specific endpoint
 @app.get("/store")
